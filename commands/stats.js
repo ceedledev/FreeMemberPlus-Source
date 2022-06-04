e5d9291008b1f51a9ca2df493eaae910929e0d7d
Ceed <82039844+ceedledev@users.noreply.github.com>
2022-06-04 15:47:12 +0200

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -18,7 +18,7 @@ module.exports = {
     .setColor("#2f3136")
 
     .setAuthor(client.user.username, client.user.displayAvatarURL())
-    .setDescription(`<:info:929201887511719946> __Informations :__\n> RAM: \``+(process.memoryUsage().rss / 1024 / 1024).toFixed(2)+`MB\`\n> Uptime: \``+uptime.join(", ")+`\`\n> Ping: \``+Math.round(client.ws.ping)+`ms\`\n> Discord.js: \``+Discord.version+`\`\n> Node.js: \``+process.versions.node+`\`\n<:info:929201887511719946> __Statistics :__\n> Serveur(s): \``+client.guilds.cache.size+`\`\n> Membre(s): \`` +client.users.cache.size+`\`\n❤️ Merci à >Nion d'avoir partager le code.`)
+    .setDescription(`<:info:929201887511719946> __Informations :__\n> RAM: \``+(process.memoryUsage().rss / 1024 / 1024).toFixed(2)+`MB\`\n> Uptime: \``+uptime.join(", ")+`\`\n> Ping: \``+Math.round(client.ws.ping)+`ms\`\n> Discord.js: \``+Discord.version+`\`\n> Node.js: \``+process.versions.node+`\`\n<:info:929201887511719946> __Statistics :__\n> Serveur(s): \``+client.guilds.cache.size+`\`\n> Membre(s): \`` +client.users.cache.size+`\`\n❤️ Merci à Ceed d'avoir partager le code.`)
      .setImage("https://images-ext-1.discordapp.net/external/4DeaYQcsYoMmeYyfLXcXJU4ClNDBUxKNha2JOIqHn1c/https/share.creavite.co/f5GvKKVgvhoLj7Uf.gif")
     message.channel.send(embed)
   }
